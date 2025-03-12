f6eec3227a7aff0587d48671f49160067e76c289
Adam Hartline <arcydrool@gmail.com>
2025-03-12 19:35:47 -0400

    adjust product version

--- src/build.rs
+++ src/build.rs
@@ -3,10 +3,11 @@ extern crate winresource;
 fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winresource::WindowsResource::new();
+        let version: u64 = 0 << 48 | 1 << 32 | 0 << 16 | 0;
         res.set_icon("images/trashdov.ico")
             .set("InternalName", "TDDESK.EXE")
             // manually set version 1.0.0.0
-            .set_version_info(winresource::VersionInfo::PRODUCTVERSION, 0x0001000000000000);
+            .set_version_info(winresource::VersionInfo::PRODUCTVERSION, version);
         res.compile().unwrap();
     }
 }
