23771c85681e5e68147dffb3b6aab74b8fcd7f1a
Adam Hartline <arcydrool@gmail.com>
2025-03-21 15:36:57 -0400

    Starts of Nest

---
+++ src/model/nest.rs
@@ -0,0 +1,17 @@
+use http::uri;
+
+struct Nest {
+    id: u128,
+    location: Uri,
+    date_created: String,
+    date_modified: String,
+    name: String,
+    organization: String,
+    location: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+impl Nest {
+
+}
